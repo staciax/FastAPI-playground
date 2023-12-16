9cf57b9317a4e77fce3f0a2824759085167f0030
STACiA <69411527+staciax@users.noreply.github.com>
2023-12-16 20:29:02 +0700

    add `CORSMiddleware ` for app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+
+# https://fastapi.tiangolo.com/tutorial/cors/
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=('*'),
+    allow_credentials=True,
+    allow_methods=('GET', 'POST', 'PUT', 'DELETE'),
+    allow_headers=('*'),
+)
 
 
 @app.get('/', include_in_schema=False)
