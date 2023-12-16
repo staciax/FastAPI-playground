8b87b3b7f6a5df3419286178a258b6273fadfdd4
STACiA <69411527+staciax@users.noreply.github.com>
2023-12-16 23:19:07 +0700

    Add CORS origins to settings

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
+from app.settings import settings
+
 app = FastAPI()
 
 
 # https://fastapi.tiangolo.com/tutorial/cors/
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=('*'),
+    allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=('GET', 'POST', 'PUT', 'DELETE'),
     allow_headers=('*'),
--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    CORS_ORIGINS: list[str]
+
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / '.env',
         env_file_encoding='utf-8',
