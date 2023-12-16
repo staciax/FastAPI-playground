05b923ef8ee0ae1dae8b1da59901889d43f2b584
STACiA <69411527+staciax@users.noreply.github.com>
2023-12-16 23:24:22 +0700

    Add OPTIONS method to CORS middleware

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=('GET', 'POST', 'PUT', 'DELETE'),
+    allow_methods=('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'),
     allow_headers=settings.CORS_HEADERS,
 )
 
